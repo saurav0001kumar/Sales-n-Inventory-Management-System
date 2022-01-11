47ce80a78622a5fc738a14977cd40936ad9b783f
saurav0001kumar <saurav0001kumar@gmail.com>
2022-01-11 11:45:45 +0530

    "scan QR to add" feature added

--- sales_n_inventory_flutter_app/lib/screens/screen4_addNewProduct.dart
+++ sales_n_inventory_flutter_app/lib/screens/screen4_addNewProduct.dart
@@ -1,13 +1,19 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sales_n_inventory_flutter_app/app_drawer/drawer.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:sales_n_inventory_flutter_app/others/otherFunctions.dart';
 
 // Create a CollectionReference that references the firestore collection
 CollectionReference inv_db =
     FirebaseFirestore.instance.collection('inventory_db');
 //
+String _scanBarcode = 'Unknown';
 
 class addNewProductScreen extends StatefulWidget {
   var totalProducts;
@@ -41,6 +47,7 @@ class addNewProductScreenState extends State<addNewProductScreen> {
   Future<void> initState() {
     // TODO: implement initState
     super.initState();
+    resetFields();
     print(totalProducts);
   }
 
@@ -107,122 +114,185 @@ class addNewProductScreenState extends State<addNewProductScreen> {
   }
 
   Widget bodyForAddNewProductScreen(context) {
-    return Form(
-        key: _formKey,
-        child: ListView(children: [
-          SizedBox(
-            height: MediaQuery.of(context).devicePixelRatio * 35,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-            child: TextFormField(
-              controller: item_nameTC,
-              keyboardType: TextInputType.text,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: InputDecoration(
-                labelText: 'Product Name',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).devicePixelRatio * 35,
+        ),
+        Container(
+            alignment: Alignment.center,
+            child: Flex(
+                direction: Axis.vertical,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                          onTap: () => scanQR(),
+                          child: Icon(
+                            MaterialCommunityIcons.qrcode_scan,
+                            size: 75,
+                          )),
+                      Text(
+                        "  OR  ",
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 18,
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: () => scanBarcodeNormal(),
+                          child: Icon(
+                            MaterialCommunityIcons.barcode_scan,
+                            size: 75,
+                          )),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 15, 10, 10),
+                    child: Center(
+                      child: Text(
+                        "Scan QR or Barcode to add.",
+                        style: TextStyle(
+                            color: Colors.grey[500],
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+
+                  // ElevatedButton(
+                  //     onPressed: () => startBarcodeScanStream(),
+                  //     child: Text('Start barcode scan stream')),
+                  Text(
+                      (_scanBarcode == "Unknown")
+                          ? ""
+                          : ((_scanBarcode == "-1") ? "" : "✔"),
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold))
+                ])),
+        Form(
+            key: _formKey,
+            child: Column(children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                child: TextFormField(
+                  controller: item_nameTC,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.sentences,
+                  decoration: InputDecoration(
+                    labelText: 'Product Name',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '*';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return '*';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-            child: TextFormField(
-              controller: categoryTC,
-              keyboardType: TextInputType.text,
-              textCapitalization: TextCapitalization.words,
-              decoration: InputDecoration(
-                labelText: 'Product Category',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                child: TextFormField(
+                  controller: categoryTC,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: InputDecoration(
+                    labelText: 'Product Category',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '*';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return '*';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-            child: TextFormField(
-              controller: brandTC,
-              keyboardType: TextInputType.text,
-              textCapitalization: TextCapitalization.words,
-              decoration: InputDecoration(
-                labelText: 'Product Brand',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                child: TextFormField(
+                  controller: brandTC,
+                  keyboardType: TextInputType.text,
+                  textCapitalization: TextCapitalization.words,
+                  decoration: InputDecoration(
+                    labelText: 'Product Brand',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '*';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return '*';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-            child: TextFormField(
-              controller: pricePerItemTC,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Price per item (INR)',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                child: TextFormField(
+                  controller: pricePerItemTC,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Price per item (INR)',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '*';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return '*';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-            child: TextFormField(
-              controller: quantityTC,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Quantity',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                child: TextFormField(
+                  controller: quantityTC,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Quantity',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '*';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return '*';
-                }
-                return null;
-              },
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 100.0, vertical: 10),
-            child: ElevatedButton.icon(
-              label: Text("Add New"),
-              icon: Icon(
-                Icons.add_circle_outline,
-                size: 30,
-              ),
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  await _addNewProduct();
-                }
-              },
-            ),
-          )
-        ]));
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 100.0, vertical: 10),
+                child: ElevatedButton.icon(
+                  label: Text("Add New"),
+                  icon: Icon(
+                    Icons.add_circle_outline,
+                    size: 30,
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      await _addNewProduct();
+                    }
+                  },
+                ),
+              )
+            ]))
+      ],
+    );
   }
 
   Future<void> _addNewProduct() {
@@ -269,5 +339,70 @@ class addNewProductScreenState extends State<addNewProductScreen> {
         .update({'inventory_last_updated_on': DateTime.now()})
         .then((value) => print("Inventory Last Date Updated"))
         .catchError((error) => print("Failed to update modified date: $error"));
+  }
+
+  //QR methods
+
+  Future<void> startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            '#ff6666', 'Cancel', true, ScanMode.BARCODE)
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+    fillFieldsAfterScan();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+    fillFieldsAfterScan();
+  }
+
+  //Reset All Fields
+  void fillFieldsAfterScan() {
+    setState(() {
+      item_nameTC.text =
+          (_scanBarcode == "-1") ? "" : _scanBarcode.split(",")[0];
+      categoryTC.text = _scanBarcode.split(",")[1];
+      brandTC.text = _scanBarcode.split(",")[2];
+      pricePerItemTC.text = _scanBarcode.split(",")[3];
+      quantityTC.text = _scanBarcode.split(",")[4];
+    });
   }
 }
